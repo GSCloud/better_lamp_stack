e9e9b087b2e956c3c3890d7b35aee92b4982e27c
Filip Oščádal <f@mxd.cz>
2021-09-08 10:10:16 +0200

    minor update

--- bin/create_pdf.sh
+++ bin/create_pdf.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 #@author Filip Oščádal <git@gscloud.cz>
 
-. bin/_includes.sh
+dir="$(dirname "$0")"
+. "$dir/_includes.sh"
 
 command -v docker >/dev/null 2>&1 || fail "Docker is NOT installed!"
 
-find . -maxdepth 1 -iname "*.md" -exec echo "converting {} to ADOC ..." \; -exec docker run --rm -v "$(pwd)":/data pandoc/core -f markdown -t asciidoc -i {} -o "{}.adoc" \;
-find . -maxdepth 1 -iname "*.adoc" -exec echo "converting {} to PDF ..." \; -exec docker run --rm -v $(pwd):/documents/ asciidoctor/docker-asciidoctor asciidoctor-pdf -a allow-uri-read -d book "{}" \;
+# MarkDown -> ADOC
+find . -type d \( -path ./node_modules -o -path ./vendor \) -prune -false -o -iname "*.md" -exec echo "Converting {} to ADOC" \; \
+    -exec docker run --rm -u $(id -u ${USER}):$(id -g ${USER}) -v "$(pwd)":/data pandoc/core -f markdown -t asciidoc -i {} -o "{}.adoc" \;
+
+# ADOC -> PDF
+find . -type d \( -path ./node_modules -o -path ./vendor \) -prune -false -o -iname "*.adoc" -exec echo "Converting {} to PDF" \; \
+    -exec docker run --rm -u $(id -u ${USER}):$(id -g ${USER}) -v $(pwd):/documents/ asciidoctor/docker-asciidoctor:1.9.0 asciidoctor-pdf "{}" \;
 
 exit 0
