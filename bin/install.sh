ac4eb67b5cbb81018da852d8682d0bde32ffa2f9
Filip Oščádal <f@mxd.cz>
2022-04-21 11:03:59 +0200

    + optimizations, extra comments; make install now runs CMD_EXTRAS1 too

--- bin/install.sh
+++ bin/install.sh
@@ -15,6 +15,7 @@ source .env
 ###############################################################################
 
 if [ -z ${APP_NAME+x} ]; then fail "Missing APP_NAME definition!"; fi
+if [ -z ${CMD_EXTRAS1+x} ]; then fail "Missing CMD_EXTRAS1 definition!"; fi
 if [ -z ${DB_NAME+x} ]; then fail "Missing DB_NAME definition!"; fi
 if [ -z ${DB_VOLUME+x} ]; then fail "Missing DB_VOLUME definition!"; fi
 if [ -z ${PMA_NAME+x} ]; then fail "Missing PMA_NAME definition!"; fi
@@ -32,9 +33,24 @@ docker-compose up -d
 docker exec $APP_NAME cp -u /usr/local/etc/php/php.ini-production /usr/local/etc/php/php.ini
 docker exec $APP_NAME pear config-set php_ini /usr/local/etc/php/php.ini
 
+# command extras #1
+if [ ! -z "${CMD_EXTRAS1}" ]; then
+    docker exec $APP_NAME ${CMD_EXTRAS1}
+fi
+
 info "Restarting container"
 docker restart $APP_NAME
 
 echo ""
+
+echo -en "APP running at: \e[1;32mhttp://localhost:${APP_PORT}\e[0m\n"
+docker exec $APP_NAME php -i | grep 'memory_limit'
+docker exec $APP_NAME php -i | grep 'upload_max_filesize'
+echo -en "\n"
+
+echo -en "ADMIN running at: \e[1;32mhttp://localhost:${PMA_PORT}\e[0m\n"
+docker exec $PMA_NAME php -i | grep 'memory_limit'
+docker exec $PMA_NAME php -i | grep 'upload_max_filesize'
+echo -en "\n"
 
 exit 0
